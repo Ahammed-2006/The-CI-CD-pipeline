2f98b32888224b64c8fe14e2006fb011a2b1bcad
Ahammed Ussain <habibhassan8489@gmail.com>
2025-09-07 14:59:06 +0530

    Update and rename demo_app.java to main_app.java

--- main_app.java
+++ main_app.java
@@ -1,11 +1,10 @@
-package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MyWebsiteApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(MyWebsiteApplication.class, args);
     }
 }
